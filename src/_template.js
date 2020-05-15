68c26d5d3245337c8dba735d080003d870fb8851
Chinmaya Pulikallu <chinmayapulikallu@gmail.com>
2020-05-15 17:02:16 -0500

    created Header and home component

---
+++ src/_template.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class _template extends Component{
+    render() {
+        return(
+            <div>
+                <h2>_template</h2>
+            </div>
+        )
+    }
+}
+
+export default _template;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import {HashRouter as Router, Route} from 'react-router-dom';
+
+//importing components to App.js
+import Header from '../Header/Header';
+import Home from '../Home/Home';
 
 class App extends Component {
   // Renders the entire app on the DOM
   render() {
     return (
       <div className="App">
-        <p>Empty Page</p>
+       <Header />
+       {/* Routes for components, Home component is the Home page */}
+       <Router>
+         <Route exact path="/" component={Home}/>
+       </Router>
       </div>
     );
   }
